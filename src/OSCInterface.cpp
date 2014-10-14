2c6c194649b24991cf873a1b8ea656ee844aaeb9
media_lab <daniell@media.mit.edu>
2014-10-14 13:59:27 -0400

    Starting computer-computer communication
    Having issues with importing the damn OSC library ...
    
    It says pipe, close, read, write are all unidentified symbols. In the example, they're referencing unistd.h in usr/include. I don't know what the issue is!

---
+++ src/OSCInterface.cpp
@@ -0,0 +1,20 @@
+//
+//  OSCInterface.cpp
+//  cooperFORM
+//
+//  Created by Tangible Media Group on 10/14/14.
+//
+//
+
+#include "OSCInterface.h"
+//OSCInterface::OSCInterface(string host, int port) {
+//    this->host = host;
+//    this->port = port;
+//}
+
+//void OSCInterface::sendModeChange(string newMode) {
+//    ofxOscMessage message;
+//    message.setAddress("/inform/mode");
+    //    message.addStringArg(newMode);
+    //    sender.sendMessage(message);
+    //}

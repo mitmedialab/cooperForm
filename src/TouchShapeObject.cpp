980cdd5fc3180034799eb1d0563ac1db7e76407e
Daniel Windham <dewindham@gmail.com>
2015-06-22 21:33:01 -0400

    la la la

--- src/TouchShapeObject.cpp
+++ src/TouchShapeObject.cpp
@@ -188,23 +188,12 @@ void TouchShapeObject::renderShape()
 
 void TouchShapeObject::renderTangibleShape(int w, int h)
 {
-    ofSetColor((int) cpHeight);
-    ofCircle(w/2, h/2, w/2);
-
-    ofSetColor((int) cpHeight + 50);
-    ofCircle(w/2, h/2, w/20);
-
-    return;
-
-    mOutputShapeImage.draw(0, 0, w, h);
-    //ofFbo pinHeightMapImage; //FBO where we render height map
-    //pinHeightMapImage.allocate(TOUCHSCREEN_VISIBLE_SIZE_X, TOUCHSCREEN_SIZE_Y, GL_RGBA);
+    float xStep = (float) w / RELIEF_PHYSICAL_SIZE_X;
+    float yStep = (float) h / RELIEF_PHYSICAL_SIZE_Y;
     for (int x = 0; x < RELIEF_PHYSICAL_SIZE_X; x++) {
-        for(int y = 0; y < RELIEF_PHYSICAL_SIZE_Y; y++){
-            int distance = ofDist(RELIEF_PHYSICAL_SIZE_X/2, RELIEF_PHYSICAL_SIZE_Y/2, x, y);
-            distance = min(15, distance);
-            //int outputHeight = ofMap(distance, 0, 15, (int) controlPinHeight, 230);
-            allPixels[RELIEF_PHYSICAL_SIZE_X * y + x] = (x + y) * 10 % 128;
+        for (int y = 0; y < RELIEF_PHYSICAL_SIZE_Y; y++) {
+            ofSetColor(allPixels[RELIEF_PHYSICAL_SIZE_X * y + x]);
+            ofRect(x * xStep, y * yStep, xStep, yStep);
         }
     }
 }
